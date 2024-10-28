e1e7797036c3ba677a8ee47bbaa3b0f174575d6c
Bhargavi <bhargavi20102005@gamil.com>
2024-10-28 23:48:52 +0530

    RelationalOperator.c

---
+++ RelationalOperator.c
@@ -0,0 +1,22 @@
+/*code for Reletional operator*/
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    a=20;
+    b=35;
+    (a>b);//a is greater than b
+    printf("%d\n",a>b);
+    (a<b);//b is greater than a
+    printf("%d\n",a<b);
+    (a==b);//a is equal to be
+    printf("%d\n",a==b);
+    (a>=b);//a is gretar than or equal to b
+    printf("%d\n",a>=b);
+    (a<=b);//b is gretar than or equal to a
+    printf("%d\n",a<=b);
+    (a!=b);//a is not equal to b
+    printf("%d\n",a!=b);
+    return 0;
+}
