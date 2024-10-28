33856845151b5fe477ef762b1e70f6aa2b594e87
Bhargavi <bhargavi20102005@gamil.com>
2024-10-28 22:07:34 +0530

    add logicaloperators.c

---
+++ logicaloperators.c
@@ -0,0 +1,18 @@
+/*code for logical operators*/
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    a=25;
+    b=5;
+    if (a<b && b>10)//logical AND example
+        {
+        printf("a is grater than b&&b is less than 10\n");
+        }
+           else
+           {
+           printf("condition is not satisfed\n");
+           }
+    
+}
