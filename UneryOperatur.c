e2e9d1b112d2a24d8605e8cca5b798816c070164
Bhargavi <bhargavi20102005@gamil.com>
2024-10-29 20:46:12 +0530

    Add UneryOperatur.c

---
+++ UneryOperatur.c
@@ -0,0 +1,15 @@
+/*Program for Unery Operatur*/
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    a=4;
+    b=5;
+    printf("%d\n",++a);//Value incremente by 1
+    printf("%d\n",++b);//Value increments by 1
+    printf("%d\n",--a);//Value decrements by 1
+    printf("%d\n",--b);//Valu decrements by 1
+    return 0;
+
+}
