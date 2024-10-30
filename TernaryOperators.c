31abaf5268a889e09ce34b38b1fbc19f1154374d
Bhargavi <bhargavi20102005@gamil.com>
2024-10-30 14:43:21 +0530

    Adding TernaryOperatours.c

---
+++ TernaryOperators.c
@@ -0,0 +1,12 @@
+/*Program for Ternary Operators*/
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    a=8;
+    b=7;
+    (b>a)?printf("b is greater than a that is %d>%d",b,a)
+    :printf("a is greater than b that is %d>%d",a,b);
+    return 0;
+}
