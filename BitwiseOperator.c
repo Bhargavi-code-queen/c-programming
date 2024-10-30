1e16ded1c8a01fac0c7183884cc377c511430b83
Bhargavi <bhargavi20102005@gamil.com>
2024-10-30 19:37:52 +0530

    BitwiseOperator.c

---
+++ BitwiseOperator.c
@@ -0,0 +1,14 @@
+/*Code for Bitwise Operator*/
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    a=12;//Binary:1100
+    b=10;//Binary:1010
+    printf("a&b = %d\n",a&b);//AND Operation
+    printf("a|b = %d\n",a|b);//OR Operation
+    printf("a^b = %d\n",a^b);//XOR Operation
+    printf("~a = %d\n",~a);//NOT Operation
+    return  0;
+}
