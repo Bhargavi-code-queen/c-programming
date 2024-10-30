a925e7ec9e1a5f73d782f8e45e618c0f04473538
Bhargavi <bhargavi20102005@gamil.com>
2024-10-30 14:26:18 +0530

    AssignmentOperators.c

---
+++ AssignmentOperators.c
@@ -0,0 +1,17 @@
+/*Program Assignment Operators*/
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    a=50;
+    a+=50;//Adding 50 to a
+    printf("%d\n",a);
+    a-=50;//Subtracting 50 from 100
+    printf("%d\n",a);
+    a*=10;//Multiplying 50*10
+    printf("%d\n",a);
+    a/=25;//Dividing 500 by 50
+    printf("%d\n",a);
+    return 0;
+}
