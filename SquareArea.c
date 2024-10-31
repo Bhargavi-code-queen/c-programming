761b5e5ab1bb57874b9cc03a360f727aaa66a00a
Bhargavi <bhargavi20102005@gamil.com>
2024-10-31 20:15:19 +0530

    SquareArea.c

---
+++ SquareArea.c
@@ -0,0 +1,12 @@
+/*Code for Area Square */
+#include<stdio.h>
+
+int main()
+{
+    float side;
+    printf("Enter side =");
+    scanf("%f",&side);
+
+    printf("Area is %f\n",side*side);
+    return 0;
+    }
