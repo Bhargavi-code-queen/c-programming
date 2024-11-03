b064861c750ce71a08ebf1e0534cf1331ba91e6d
Bhargavi <bhargavi20102005@gamil.com>
2024-11-03 18:55:29 +0530

    calculatingCube.c

---
+++ calculatingCube.c
@@ -0,0 +1,16 @@
+/*Code for Calculating cube*/
+#include<stdio.h>
+
+int main()
+{
+
+    float n,cube;
+    printf("Enter number = ");
+    scanf("%f",&n);
+    /*calculating cube */
+    cube = n*n*n;
+    /*printing cube */
+    printf("cube is %f\n",cube);
+    return 0;
+
+}
