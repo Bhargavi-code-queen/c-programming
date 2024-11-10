09be18ec1a0a42e67eeb596434eba620289fe7e8
Bhargavi <bhargavi20102005@gamil.com>
2024-11-10 15:04:49 +0530

    Averageof3.c

---
+++ Averageof3.c
@@ -0,0 +1,15 @@
+/*Programme for average of three numbers*/
+#include<stdio.h>
+
+int main()
+{
+    float n,average;
+    printf("Enter a number:-");
+    scanf("%f/n",&n);
+    /*Calculating average*/
+    average = n+n+n/3;
+    /*printing average*/
+    printf("average is:-%f/n ",average);
+    return 0;
+
+}
